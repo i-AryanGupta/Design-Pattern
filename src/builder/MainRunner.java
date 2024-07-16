e8f74a809865fba3bc60984e26f21282d06591ec
i-AryanGupta <aryangupta6008@gmail.com>
2024-07-16 17:02:13 +0530

    Builder design pattern Done

---
+++ src/builder/MainRunner.java
@@ -0,0 +1,15 @@
+package builder;
+
+public class MainRunner {
+	public static void main(String[] args) {
+		Vehicle car = new Vehicle.VehicleBuilder("V8", 4).setAirbags(6).build();
+		Vehicle bike  = new Vehicle.VehicleBuilder("650cc", 2).build();
+		
+		System.out.println(" Car engine: "+car.getEngine()+" Seats: "+car.getSeats()+ " Airbags "+ car.getAirbags());
+		System.out.println(" Car engine: "+bike.getEngine()+" Seats: "+bike.getSeats()+ " Airbags "+ bike.getAirbags());
+		
+		
+		
+	}
+
+}
--- src/builder/Vehicle.java
+++ src/builder/Vehicle.java
@@ -2,4 +2,53 @@ package builder;
 
 public class Vehicle {
 
+	private String engine;
+	private int seats;
+	
+	//optional
+	private int airbags;
+
+	public Vehicle(VehicleBuilder builder) {
+		this.engine = builder.engine;
+		this.seats = builder.seats;
+		this.airbags = builder.airbags;
+	}
+
+	public String getEngine() {
+		return engine;
+	}
+
+	public int getSeats() {
+		return seats;
+	}
+
+	public int getAirbags() {
+		return airbags;
+	}
+	
+	public static class VehicleBuilder{
+		
+		private String engine;
+		private int seats;
+		
+		private int airbags;
+
+		public VehicleBuilder(String engine, int seats) {
+			super();
+			this.engine = engine;
+			this.seats = seats;
+		}
+
+		public VehicleBuilder setAirbags(int airbags) {
+			this.airbags = airbags;
+			return this;
+		}
+		
+		public Vehicle build() {
+		return new Vehicle(this);
+		}	
+		
+	}
+	
+	
 }
