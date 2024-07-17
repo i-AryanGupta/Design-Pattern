302309ead856856d803d06cd163ab3c98da4dad3
i-AryanGupta <aryangupta6008@gmail.com>
2024-07-17 12:54:53 +0530

    Singleton pattern done

---
+++ src/singleton/MainRun.java
@@ -0,0 +1,11 @@
+package singleton;
+
+public class MainRun {
+	public static void main(String[] args) {
+		
+		Singleton singleton = Singleton.getInstance();
+		System.out.println(singleton);
+	}
+	
+
+}
